b4df81e61d3e85409f915f72055647bb7a5a6294
BenediktWeissenbach <benedikt.weissenbach@stud.fh-campuswien.ac.at>
2021-04-17 19:18:10 +0200

    Singleton

--- src/main/java/MCP.java
+++ src/main/java/MCP.java
@@ -2,8 +2,22 @@ import trafficlight.ctrl.TrafficLightCtrl;
 
 
 public class MCP {
-    public static void main(String[] args) {
+
+    //https://mattcarroll.medium.com/singletons-the-safe-way-9d1e019220fc
+    public static void main(String[] args) {}
+
+    private MCP(){
         TrafficLightCtrl ctrl = new TrafficLightCtrl();
         ctrl.run();
     }
+
+    private static MCP instance;
+
+    public static MCP getInstance(){
+        if(null == MCP.instance){
+            MCP.instance = new MCP();
+        }
+        return MCP.instance;
+    }
+
 }
